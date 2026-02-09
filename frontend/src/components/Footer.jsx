47411f4aa29c3db3a84a69d18bb86d2f72415d69
Subhajit Paul <palsubhajit612@gmail.com>
2026-02-09 20:13:07 +0530

    Update footer content and contact information
    update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,10 +8,8 @@ const Footer = () => {
         <div>
           <img className="mb-5 w-40" src={assets.logo} alt="" />
           <p className="w-full md:w-2/3 text-gray-600 leading-6">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen
+            Prescripto is a smart healthcare appointment platform that helps patients connect with trusted doctors easily.
+Book appointments, consult specialists, and manage your healthcare needs securely — all in one place.
           </p>
         </div>
         {/* .......centre............. */}
@@ -30,14 +28,14 @@ const Footer = () => {
           <p className="text-xl font-medium mb-5 ">GET IN TOUCH</p>
           <ul className="flex flex-col gap-2 text-gray-600">
             <li>+0-000-000-000</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>prescripto123@gmail.com</li>
           </ul>
         </div>
       </div>
       {/* ............Copyright Text............ */}
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">Copyright 2025 @ Subho.dev - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright 2026 @ Subho.dev - All Right Reserved.</p>
       </div>
     </div>
   );
